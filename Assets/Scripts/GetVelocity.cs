0454b2bf21d30b29d1313160c0c93081bfe29e63
MasahiroOta <masahiro.ktpc@gmail.com>
2018-09-06 01:01:50 +0900

    機体の速度と主翼の角度を取得する処理を作成

---
+++ Assets/Scripts/GetVelocity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetVelocity : MonoBehaviour {
+	public float time;
+    public float Vx;
+    public float Vy;
+    public float Vz;
+    public float latestX;
+    public float latestY;
+	public float latestZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getVelocity ();
+		//timer();
+	}
+
+	void getVelocity(){
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		Vz = (this.gameObject.transform.localPosition.z - latestZ) / Time.deltaTime;
+		Vy = (this.gameObject.transform.localPosition.y - latestY) / Time.deltaTime;
+        Vx = (this.gameObject.transform.localPosition.x - latestX) / Time.deltaTime;
+        latestZ = this.gameObject.transform.localPosition.z;
+		latestY = this.gameObject.transform.localPosition.y;
+        latestX = this.gameObject.transform.localPosition.x;
+    }
+
+	void timer(){
+		time += Time.deltaTime;
+		if (time >= 1.0f) {
+			Debug.Log ("y:"+Vy+", z:"+Vz);
+			time = 0.0f;
+		}
+	}
+}
---
+++ Assets/Scripts/MainWingForce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainWingForce : MonoBehaviour {
+    GetVelocity aircraft;
+    Vector3 angle;
+    //public float ;
+
+	// Use this for initialization
+	void Start () {
+        aircraft = gameObject.GetComponentInParent<GetVelocity>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //GetParentVelocity();
+        MtAngle();
+    }
+
+    void GetParentVelocity() {
+        Debug.Log("x:"+aircraft.Vx+", y:" + aircraft.Vy + ", z:" + aircraft.Vz);
+    }
+
+    void MtAngle() {
+        angle = gameObject.transform.localEulerAngles;
+        Debug.Log("x" + (angle.x));
+    }
+}
